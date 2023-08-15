a9f441bd76f3d3600f32e70f8001ba5c660b15fc
David Brandstetter <dbrandstetter0@gmail.com>
2023-08-15 22:31:23 +0200

    commented code

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ use crate::conversion::*;
 
 mod conversion;
 
+// Declare the temperature minima
 const CELSIUS_MIN: f64 = -273.15;
 const FAHRENHEIT_MIN: f64 = -459.67;
 const KELVIN_MIN: f64 = 0.0;
 
+// Declare the temperature symbols
 const CELSIUS_SYMBOL: &str = "C";
 const FAHRENHEIT_SYMBOL: &str = "F";
 const KELVIN_SYMBOL: &str = "K";
@@ -19,23 +21,32 @@ const KELVIN_SYMBOL: &str = "K";
 fn main() {
 	greet_user();
 
+	// Get the units used for conversion
 	let initial_unit: String = get_unit("initial".to_string());
 	let target_unit: String = get_unit("target".to_string());
+
+	// Create storage for input and converted degrees
 	let mut temp_initial: f64;
 	let mut temp_converted: f64;
 
+	// Let the user convert as many temperatures as wanted
 	loop {
+		// Prompt user for degree value to convert from
 		temp_initial = get_initial_temp(initial_unit.to_string());
 
+		// Calculate the converted degree value
 		temp_converted = convert_temperature(initial_unit.to_string(), target_unit.to_string(), temp_initial);
 
 		println!("{temp_initial}{initial_unit} equates to {temp_converted}{target_unit}\n");
 	}
 }
 
+// Used for choosing right method for converting two temperatures
 fn convert_temperature(initial_unit: String, target_unit: String, temp_initial: f64) -> f64 {
+	// Filter by initial unit
 	return match initial_unit.as_str() {
 		CELSIUS_SYMBOL => {
+			// Filter by initial unit
 			match target_unit.as_str() {
 				FAHRENHEIT_SYMBOL => celsius_to_fahrenheit(temp_initial),
 				KELVIN_SYMBOL => celsius_to_kelvin(temp_initial),
@@ -45,6 +56,7 @@ fn convert_temperature(initial_unit: String, target_unit: String, temp_initial: 
 			}
 		}
 		FAHRENHEIT_SYMBOL => {
+			// Filter by initial unit
 			match target_unit.as_str() {
 				CELSIUS_SYMBOL => fahrenheit_to_celsius(temp_initial),
 				KELVIN_SYMBOL => fahrenheit_to_kelvin(temp_initial),
@@ -54,6 +66,7 @@ fn convert_temperature(initial_unit: String, target_unit: String, temp_initial: 
 			}
 		}
 		KELVIN_SYMBOL => {
+			// Filter by initial unit
 			match target_unit.as_str() {
 				CELSIUS_SYMBOL => kelvin_to_celsius(temp_initial),
 				FAHRENHEIT_SYMBOL => kelvin_to_fahrenheit(temp_initial),
@@ -77,26 +90,32 @@ fn greet_user() {
 }
 
 
-// Prompts user for temperature unit and validates input
+// This function prompts the user for temperature unit and validates input
 fn get_unit(input: String) -> String {
+	// Temp variable for user input
 	let mut unit: String = String::new();
 
+	// Continue prompting until a valid input is received
 	while invalid_unit(unit.to_string().trim().to_lowercase()) {
 		println!("Please select the {} unit:\n[C] Celsius, [F] Fahrenheit, [K] Kelvin", input);
 
+		// As input gets appended, the string has to be cleared
 		unit.clear();
 
+		// Fill unit with user input
 		io::stdin()
 			.read_line(&mut unit)
 			.expect("Failed to read line")
 			.to_string();
 	}
 
+	// Returns normed input
 	return get_unit_symbol(unit.to_string().trim().to_lowercase());
 }
 
-
+// Used for extracting the unit symbol out of the users input
 fn get_unit_symbol(input: String) -> String {
+	// Check for contained unit names and starting letters
 	if input.contains("celsius") || input.starts_with("c") {
 		return CELSIUS_SYMBOL.to_string();
 	} else if input.contains("kelvin") || input.starts_with("k") {
@@ -108,8 +127,9 @@ fn get_unit_symbol(input: String) -> String {
 	return "".to_string();
 }
 
-
+// Determines whether a given string is a valid unit input
 fn invalid_unit(input: String) -> bool {
+	// Check for contained unit names and starting letters and informs user of successful selection
 	if input.contains("celsius") || input.starts_with("c") {
 		println!("-> Unit [Celsius] selected.\n");
 		return false;
@@ -124,32 +144,42 @@ fn invalid_unit(input: String) -> bool {
 	return true;
 }
 
+// This function lets the user type in the selected degree value to convert from
 fn get_initial_temp(unit: String) -> f64 {
+	// Create temp for storing user input
 	let mut input: String = String::new();
 
+	// Run until a valid input is received
 	'getting_degrees: loop {
 		println!("Please input degrees {}:", if unit == "F" { "Fahrenheit".to_string() } else if unit == "K" { "Kelvin".to_string() } else if unit == "C" {
 			"Celsius".to_string()
 		} else { "".to_string() });
 
+		// As input gets appended, the string has to be cleared
 		input.clear();
 
+		// Fill input with user input
 		io::stdin()
 			.read_line(&mut input)
 			.expect("Failed to read line")
 			.to_string();
 
+		// Parse the input to a float
 		let value: f64 = input.trim().replace(",", ".").parse().unwrap();
 
+		// Check degrees for validity
 		if valid_degree_value(unit.to_string(), value) {
 			break 'getting_degrees;
 		}
 	}
 
-	return input.trim().parse().unwrap();
+	// Return parsed and cleaned input
+	return input.trim().replace(",", ".").parse().unwrap();
 }
 
+// Used for validating degree values
 fn valid_degree_value(unit: String, value: f64) -> bool {
+	// No temperatures below absolute zero are possible - inform user if wrongly typed
 	if value < FAHRENHEIT_MIN && unit == "F" {
 		eprintln!("Fahrenheit temperatures cannot be lower than {FAHRENHEIT_MIN}!\nYour input: {value}\n");
 		return false;
